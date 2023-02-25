3434682c27aba5294ea6ac612736e118a7ff2447
Dan Mikita <dan.mikita@devsoperative.com>
2023-02-25 14:14:58 -0500

    Create post-install.sh

---
+++ .devcontainer/post-install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+DEBIAN_FRONTEND=noninteractive
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends apt-utils dialog dnsutils httpie wget unzip curl jq terraform
+DEBIAN_FRONTEND=dialog
